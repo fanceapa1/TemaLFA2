879f29153b2350a9774f00e0b554e8e6e8d5ce9f
fanceapa1 <mosultudor@gmail.com>
2026-05-10 11:07:52 +0300

    Add tests

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <fstream>
 #include <iostream>
 #include <limits>
@@ -77,5 +78,66 @@ int main() {
         cout << "RESPINS";
     }
 
+    return 0;
+} */
+
+
+
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "AutomatLambdaNFA.h"
+
+using namespace std;
+
+int main() {
+    ifstream fin("../input.txt");
+
+    int numStates = 0, numFinalStates = 0, numTransitions = 0, numWords = 0;
+    set <string> states, finalStates;
+    vector <string> transitions;
+    string initialState;
+
+    fin >> numStates;
+    for (int i=0; i<numStates; i++) {
+        string currState;
+        fin >> currState;
+        states.insert(currState);
+    }
+
+    fin >> numTransitions;
+    fin >> ws;
+    for (int i=0; i<numTransitions; i++) {
+        string currTransition;
+        getline(fin, currTransition);
+        transitions.push_back(currTransition);
+    }
+    fin >> initialState;
+    fin >> numFinalStates;
+
+    for (int i=0; i<numFinalStates; i++) {
+        string currFinalState;
+        fin>>currFinalState;
+        finalStates.insert(currFinalState);
+    }
+
+    AutomatLambdaNFA LambdaNFA(numStates, states, numTransitions, transitions, initialState, numFinalStates, finalStates);
+    LambdaNFA.printAlphabet();
+    AutomatDFA DFA = LambdaNFA.transformToDFA();
+
+    fin >> numWords;
+    for (int i=0; i<numWords; i++) {
+        string currWord;
+        fin >> currWord;
+        if (DFA.wordIsAccepted(currWord)) {
+            cout << "DA" << endl;
+            DFA.printAcceptingPath(currWord);
+        }
+        else cout << "NU" << endl << endl;
+    }
+    cout << LambdaNFA.toRegularExpression();
     return 0;
 }
